1cb5051db57c3d3e191f666c4cf91f73085163f8
Fabio D7 Fatec <fabio.dias7@fatec.sp.gov.br>
2020-10-14 17:06:59 -0300

    feat: Layout página detalhes orfanato

---
+++ public/scripts/page-orphanage.js
@@ -0,0 +1,46 @@
+const options = {
+    dragging: false,
+    touchZoom: false,
+    doubleClickZoom: false,
+    scrollWheelZoom: false,
+    zoomControl: false
+}
+
+const map = L.map('mapid', options).setView([-27.222633, -49.6455874], 15);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+//create icon
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+});
+
+
+L.marker([-27.222633, -49.6455874], {icon}).addTo(map);
+
+
+/*Image Gallery*/
+function selectImage(event) {
+    const button = event.currentTarget; //Botão que disparou o evento
+
+    //remover todas as classes .active
+    const buttons = document.querySelectorAll(".images button");
+    buttons.forEach(removeActiveClass);
+
+    function removeActiveClass(button) {
+        button.classList.remove("active");
+    }
+    
+    //selecionar a imagem clicada
+    const image = button.children[0];
+    const imageContainer = document.querySelector(".orphanage-details > img");
+
+    //atualizar o container de imagem
+    imageContainer.src = image.src;
+
+    //Adicionar a classe .active para o botão que chamou a função
+    button.classList.add("active");
+}
